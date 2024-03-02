b7b438a05ca6f819e63eb2e691d5a2030594bd46
anthony <anthonygarcia2021@gmail.com>
2024-03-02 16:02:57 -0600

    feat: login.

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,16 @@
 const BASE_URL = "https://fakestoreapi.com";
+
+export const login = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error("There was an error /POST login", error);
+  }
+};
